78f1cb69c8c7be3bca352e67b08deacea9f3f781
Haraksh Singh <harakshbrar@gmail.com>
2024-03-24 13:37:42 +0530

    permutation using recursion and swap method

---
+++ permutation_recursion.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class permutation_recursion {
+        private static void permute(int[] nums, int start, List<List<Integer>> result) {
+            if (start == nums.length) {
+                List<Integer> permutation = new ArrayList<>();
+                for (int num : nums) {
+                    permutation.add(num);
+                }
+                result.add(permutation);
+            } else {
+                for (int i = start; i < nums.length; i++) {
+                    swap(nums, start, i);
+                    permute(nums, start + 1, result);
+                    swap(nums, start, i);
+                }
+            }
+        }
+
+        private static void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            permute(nums, 0, result);
+            return result;
+        }
+}
